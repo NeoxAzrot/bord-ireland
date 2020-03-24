c8d91cf880a618b84c1c993b04f9cb6c1aa9178b
NeoxAzrot1 <sami.lepays@gmail.com>
2020-03-24 10:08:46 +0100

    Mot clés js

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php
+
+    // Ouverture de la session et initialisation des erreurs et des includes
+    session_start();
+
+    ini_set('display_errors', 'on');
+    ini_set('display_startup_errors', 'on');
+    error_reporting(E_ALL);
+
+    include 'assets/php/connect_PDO.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bord'Irlande - BLOG</title>
+        <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700,900&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/f69c2bce58.js" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <h1>Contact</h1>
+
+        <!-- Menus -->
+        <?php include 'assets/php/menu.php'; ?>
+        <?php include 'assets/php/btnConnexion.php'; ?>
+        
+        <script src="assets/js/script.js"></script>
+    </body>
+
+</html>
